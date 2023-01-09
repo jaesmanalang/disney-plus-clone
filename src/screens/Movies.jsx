fc13dc9a96f8adae26d33963cdfe47fc5fac348c
James Manalang <james@MacBook-Air.local>
2023-01-09 21:04:31 +0800

    Update favicon and page title. Apply getMovieDetails on movies and tvshows page to fetch show with image

--- src/screens/Movies.jsx
+++ src/screens/Movies.jsx
@@ -4,14 +4,26 @@ import useFetch from '../hooks/useFetch';
 import SplashScreen from '../components/SplashScreen';
 import Banner from '../components/Banner';
 import RowCards from '../components/RowCards';
+import { getMovieDetails } from '../util/api';
 
 export default function Movies() {
   const [featuredMovie, setFeaturedMovie] = useState(null);
   const { data, isLoading, error } = useFetch(request.movies.popular);
 
   useEffect(() => {
-    const random = Math.floor(Math.random() * data.length - 1);
-    setFeaturedMovie(data[random]);
+    if (data && data.length !== 0) {
+      async function loadMovieDetails() {
+        const random = Math.floor(Math.random() * data.length - 1);
+        const movie = await getMovieDetails(data[random]?.id);
+        setFeaturedMovie(movie);
+      }
+
+      loadMovieDetails();
+    }
+
+    return () => {
+      setFeaturedMovie(null);
+    };
   }, [data]);
 
   if (isLoading) {
--- src/screens/TvShows.jsx
+++ src/screens/TvShows.jsx
@@ -4,14 +4,30 @@ import useFetch from '../hooks/useFetch';
 import SplashScreen from '../components/SplashScreen';
 import Banner from '../components/Banner';
 import RowCards from '../components/RowCards';
+import { getMovieDetails } from '../util/api';
 
 export default function TvShows() {
   const [featuredMovie, setFeaturedMovie] = useState(null);
-  const { data, isLoading, error } = useFetch(request.tvShows.popular);
+  const { data, isLoading, error } = useFetch(request.tvShows.topRated);
 
+  // useEffect(() => {
+  //   const random = Math.floor(Math.random() * data.length - 1);
+  //   setFeaturedMovie(data[random]);
+  // }, [data]);
   useEffect(() => {
-    const random = Math.floor(Math.random() * data.length - 1);
-    setFeaturedMovie(data[random]);
+    if (data && data.length !== 0) {
+      async function loadMovieDetails() {
+        const random = Math.floor(Math.random() * data.length - 1);
+        const movie = await getMovieDetails(data[random]?.id, 'tv');
+        setFeaturedMovie(movie);
+      }
+
+      loadMovieDetails();
+    }
+
+    return () => {
+      setFeaturedMovie(null);
+    };
   }, [data]);
 
   if (isLoading) {
